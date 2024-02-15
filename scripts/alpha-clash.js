ec928c28893a736fb2364e2e74b0167daa3b016b
codeamin11 <legacyofalamin@gmail.com>
2024-02-15 10:18:58 +0600

    life and score number changing by js

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -11,10 +11,61 @@
 
 // }
 
+document.addEventListener('keyup', function(event){
+    const playerPressed = event.key;
+    console.log('player pressed', playerPressed);
+
+    // get the expected to be pressed
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log(playerPressed, currentAlphabet);
+    
+    // check matched or not
+    if(playerPressed === expectedAlphabet){
+        console.log('you got a point');
+        // update score:
+        // 1. get the current score
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+
+        console.log(currentScore);
+        // 2. increase the score by 1
+        const newScore = currentScore + 1;
+        // 3. show the updated score
+        currentScoreElement.innerText = newScore;
+
+
+
+        // start a new round
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    } else{
+        console.log('you missed. you lost a life');
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        // console.log('your life decreased to', currentLifeText)
+        const currentLife = parseInt(currentLifeText);
+
+        const newLife = currentLife - 1;
+        console.log('remaining life', newLife)
+        currentLifeElement.innerText = newLife;
+    }
+
+})
+
 function continueGame(){
     // step-1: generate a random alphabet
     const alphabet = getARandomAlphabet();
-    console.log('your random alphabet', alphabet);
+    // console.log('your random alphabet', alphabet);
+
+    // set randomly generated alphabet to the screen (show it)
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    // set background color
+    setBackgroundColorById(alphabet);
 }
 
 function play(){
